301f941eea5600fe0e436f85730a739ef763b0d4
Evgeny Guzhikhin <gpont97@gmail.com>
2021-10-30 19:07:53 +0300

    fix husky and tsc

--- .lintstagedrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  "./**/*.ts": [
-    "npm run lint:eslint",
-    "npm run lint:prettier",
-    "npm run lint:eslint",
-    "npm run lint:types",
-    "npm run test:related",
-  ],
-};
