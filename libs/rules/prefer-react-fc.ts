03e06dfd17ebfd23b9b43586fe22f95cca24b04f
Евгений Гужихин <gpont97@gmail.com>
2022-01-09 20:39:04 +0300

    Feat/15 eslint jsx (#16)

--- libs/rules/prefer-react-fc.ts
+++ libs/rules/prefer-react-fc.ts
@@ -1,26 +1,29 @@
-import { Rule } from "eslint";
+import { Rule } from 'eslint';
 // eslint-disable-next-line import/no-unresolved
-import { Node } from "estree";
+import { Node } from 'estree';
 
 // TODO read https://eslint.org/docs/developer-guide/working-with-rules
 export const prefereReactFC: Rule.RuleModule = {
   meta: {
-    type: "suggestion",
+    type: 'suggestion',
     docs: {
-        description: "disallow unnecessary semicolons", // TODO
-        category: "Possible Errors", // TODO
-        recommended: true,
-        url: "https://github.com/gpont/eslint-plugin-typescript-react-fc/blob/main/docs/rules/prefer-react-fc.md"
+      description: 'prefer React.FC type for props typing',
+      recommended: true,
+      url: 'https://github.com/gpont/eslint-plugin-typescript-react-fc/blob/main/docs/rules/prefer-react-fc.md',
     },
-    fixable: "code", // TODO
-    schema: [] // TODO
+    fixable: 'code', // TODO
+    // TODO hasSuggestions: true
+    schema: [], // TODO
+    messages: {
+      haveTo: 'You have to use React.FC',
+    },
   },
   create: (context) => ({
-    "VariableDeclaration ArrowFunctionExpression ReturnStatement": (node: Node) => {
+    'VariableDeclaration ArrowFunctionExpression ReturnStatement': (node: Node) => {
       // context.report({
       //   node,
-      //   message: 'You have to use React.FC'
-      // })
-    }
-  })
+      //   messageId: 'haveTo',
+      // });
+    },
+  }),
 };
--- tests/tester.ts
+++ tests/tester.ts
@@ -1,8 +1,11 @@
-import {RuleTester} from 'eslint';
+import { RuleTester } from 'eslint';
 
 export const tester = new RuleTester({
   parser: require.resolve('@typescript-eslint/parser'),
   parserOptions: {
     ecmaVersion: 2021,
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
 });
