7bf69589754f976a5c32f72b1bc904dd9170aafe
Evgeny Guzhikhin <gpont97@gmail.com>
2022-01-19 06:21:13 +0300

    add some selecor code

--- libs/rules/prefer-react-fc.ts
+++ libs/rules/prefer-react-fc.ts
@@ -1,6 +1,38 @@
 import { Rule } from 'eslint';
 // eslint-disable-next-line import/no-unresolved
-import { Node } from 'estree';
+import { Node, ArrowFunctionExpression } from 'estree';
+import { Statement, VariableDeclaration } from 'typescript';
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+const getParentArrowFuncFromReturnStatement = (node) => {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  let { parent } = node;
+  while (parent.type !== 'ArrowFunctionExpression') {
+    parent = parent.node;
+  }
+};
+
+/**
+ * ArrowFunctionExpression.parent === VariableDeclarator
+ * @param node ArrowFunctionExpression
+ */
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+const getTypeNameObject = (node) => node.parent.id.typeAnnotation.typeAnnotation.typeName;
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+const isReactFC = (node) =>
+  node.type === 'TSQualifiedName' && node.left.name === 'React' && node.right.name === 'FC';
+
+/**
+ * @param node ReturnStatement
+ */
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+const isComponent = (node) => node.argument.type === 'JSXElement';
 
 // TODO read https://eslint.org/docs/developer-guide/working-with-rules
 export const prefereReactFC: Rule.RuleModule = {
@@ -19,11 +51,17 @@ export const prefereReactFC: Rule.RuleModule = {
     },
   },
   create: (context) => ({
-    'VariableDeclaration ArrowFunctionExpression ReturnStatement': (node: Node) => {
-      // context.report({
-      //   node,
-      //   messageId: 'haveTo',
-      // });
+    // 'VariableDeclaration ArrowFunctionExpression ReturnStatement JsxElement': (node: Rule.Node) => {
+    'VariableDeclaration ArrowFunctionExpression ReturnStatement': (node: Rule.Node) => {
+      // const returnStatement = node.parent;
+      // const test = isReactFC(getTypeNameObject(returnStatement));
+
+      console.log({ node, isComponent: isComponent(node) });
+
+      context.report({
+        node,
+        messageId: 'haveTo',
+      });
     },
   }),
 };
